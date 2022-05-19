5cff5871109c397c233f9dc724135b5217d572de
Asaf Yacobovich <asafyaco@gmail.com>
2022-05-20 00:17:10 +0300

    TASK-70 Completed: Redesign popover

--- src/pages/game/components/pop-over/pop-over.component.jsx
+++ src/pages/game/components/pop-over/pop-over.component.jsx
@@ -1,5 +1,5 @@
 import "./pop-over.component.scss";
-import { Popover } from "@mui/material";
+import { Button, Popover } from "@mui/material";
 const PopOverComponent = (props) => {
   return (
     <div className="qcg-pop-over">
@@ -8,16 +8,25 @@ const PopOverComponent = (props) => {
         open={props.isOpen}
         anchorEl={props.anchorEl}
         onClose={(e) => props.handleClose(e)}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
       >
         <div>
           {props.categoryGroups?.map((c, index) => {
             return (
-              <div
-                className="item"
-                onClick={() => props.onCategoryGroupClick(props.id, c)}
-                key={index}
-              >
-                {c}
+              <div className="item" key={index}>
+                <Button
+                  variant="contained"
+                  onClick={() => props.onCategoryGroupClick(props.id, c)}
+                  >
+                  {c}
+                </Button> 
               </div>
             );
           })}
